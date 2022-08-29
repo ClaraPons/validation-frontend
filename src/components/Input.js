6ee5ba96f14e349d823cb0477b5f732f0daa1d42
ClaraPons <claragapons@gmail.com>
2022-08-29 12:31:56 +0200

    modifications

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Input = ({label, type, value, placeholder, handleChange, error, required}) => {
+const Input = ({label, type, value, placeholder, handleChange, error}) => {
 
     // console.log({error.msg})
 
@@ -9,7 +9,7 @@ const Input = ({label, type, value, placeholder, handleChange, error, required})
         <section className="flex justify-center py-0.5">
             <div className="py-4 bg-amber-50 px-5 rounded">
                 <label className="block mb-2 text-m ">{label}</label>
-                <input className="border-2 border-amber-100 rounded w-[600px] p-1 focus:outline-none focus:border-amber-400 required:border-red-300" type={type} value={value} placeholder={placeholder} onChange={handleChange} required={required}/>
+                <input className={`border-2 border-amber-100 rounded w-[600px] p-1 focus:outline-none focus:border-amber-400 ${error ? ('border-red-300') : ('') }`} type={type} value={value} placeholder={placeholder} onChange={handleChange} />
                 {error && <p className="flex m-t-10 text-red-500 text-sm">{error.map(err=>err.msg)}</p>}
             </div>
         </section>
